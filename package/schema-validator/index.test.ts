76ca4db0e9599ab6d5655c20e2e5898cdd461c44
polaski0 <meynard.trinidad44@gmail.com>
2024-07-30 00:54:46 +0800

    Minor fixes in validation flow

--- package/schema-validator/index.test.ts
+++ package/schema-validator/index.test.ts
@@ -1,46 +1,63 @@
 import { v } from "."
 
-describe("string", () => {
+describe.skip("string", () => {
     const strSchema = v.string();
 
     it("should return true", () => {
-        const result = strSchema.parse("asdf")
+        const result = strSchema.validate("asdf")
         expect(result.valid).toBe(true)
-    })
-
-    it("should return false", () => {
-        const result = strSchema.parse()
-        expect(result.valid).toBe(false)
     })
 
     it("should allow undefined value and return true", () => {
         const strSchema = v.string().optional();
-        const result = strSchema.parse()
+        const result = strSchema.validate()
         expect(result.valid).toBe(true)
+    })
+
+    it("should throw ValidationError on empty input", () => {
+        expect(() => strSchema.validate()).toThrow(v.ValidationError)
+    })
+
+    it("should throw ValidationError on wrong input type", () => {
+        expect(() => strSchema.validate(1)).toThrow(v.ValidationError)
+    })
+
+    it("should throw ValidationError on optional but wrong input type", () => {
+        const strSchema = v.string().optional();
+        expect(() => strSchema.validate(1)).toThrow(v.ValidationError)
     })
 })
 
 describe("object", () => {
     const objSchema = v.object({
-        name: v.string(),
+        name: v.string().min(2),
         address: v.object({
             street: v.string()
         }),
     });
 
-    it("should return true", () => {
-        const result = objSchema.parse({
+    it.skip("should return true", () => {
+        const result = objSchema.validate({
             name: "John",
             address: {
                 street: "123"
             }
         })
-
+        console.log("Object Result", result)
         expect(result.valid).toBe(true)
     })
 
-    it("should return false", () => {
-        const result = objSchema.parse({})
-        expect(result.valid).toBe(false)
+    it("should throw ValidationError on empty object", () => {
+        try {
+            objSchema.validate({
+                name: "J"
+            })
+        } catch (err) {
+            if (err instanceof v.ValidationError) {
+                console.log(err._result)
+            }
+        }
+
+        expect(() => objSchema.validate({})).toThrow(v.ValidationError)
     })
 })
--- package/schema-validator/index.ts
+++ package/schema-validator/index.ts
@@ -1,11 +1,11 @@
 type ParseInput = {
-    input: unknown;
+    input: any;
 }
 
 type ParseReturn = {
     input: unknown;
     valid: boolean;
-    issues?: Issue[];
+    issues: Issue[];
 }
 
 type Rule = {
@@ -26,39 +26,92 @@ type Issue = {
 }
 
 
-abstract class Schema<Input, Output> {
+abstract class Schema<Output = unknown> {
+    _type: string;
+    _typeCheck: (v: unknown) => boolean;
+
     _rules: Rule[];
     _options: Options;
-    _issues?: Issue[];
+    _issues: Issue[];
 
-    abstract _parse(args: ParseInput): ParseReturn
+    // Fix _parse abstract method due to following reasons:
+    //  -   Needs to call the _typeCheck method in all _parse method, causing
+    //      redundancy and more error prone when being extended
+    //  -   Each _parse method that does not require recursive parsing capability
+    //      such as string, number, etc. will redundantly call the same function
+    //      that goes through the _rules array just to check the method
+    //
+    //  In short, create a better function that can be used globally without the subclasses
+    //  needing to repeat the same methods over and over again
+    // abstract _parse(args: ParseInput): ParseReturn
 
-    constructor() {
+    constructor({
+        type,
+        check
+    }: {
+        type: string,
+        check: (v: unknown) => boolean;
+    }) {
         this._rules = []
+        this._issues = []
+        this._type = type
+        this._typeCheck = check
         this._options = {
             required: true
         }
-
-        this.required()
     }
 
-    parse(input?: unknown) {
+    _parse(args: ParseInput): ParseReturn {
+        const result: ParseReturn = {
+            valid: true,
+            input: args.input,
+            issues: []
+        }
+
+        if (!this.isType(args.input)) {
+            this._addIssue({
+                message: `Invalid input`,
+                name: "invalid type",
+                found: args.input,
+            })
+        }
+
+        for (const rule of this._rules) {
+            if (!rule.cb(args.input)) {
+                this._addIssue({
+                    message: rule.message,
+                    name: rule.name,
+                    found: args.input,
+                })
+            }
+        }
+
+        result.issues = this._issues
+        if (result.issues.length) {
+            result.valid = false
+        }
+        return result;
+    }
+
+    isType(input: unknown) {
+        if (!input) {
+            if (!this._options.required && input === null) return true;
+            if (!this._options.required && input === undefined) return true;
+        }
+        return this._typeCheck(input)
+    }
+
+    validate(input?: unknown) {
         const result = this._parse({ input })
-        return result
-    }
-
-    required() {
-        this._options.required = true;
-        return this._addRule({
-            name: "required",
-            cb: (value) => value !== undefined && value !== null && value !== "",
-            message: "Required",
-        });
+        if (!result.valid) {
+            throw new ValidationError(result)
+        }
+        return result;
     }
 
     optional() {
         this._options.required = false;
-        return this._removeRule("required")
+        return this;
     }
 
     _addIssue(issue: Issue) {
@@ -74,61 +127,72 @@ abstract class Schema<Input, Output> {
         this._rules.push(rule);
         return this;
     }
-
-    _removeRule(name: string) {
-        const index = this._rules.findIndex(rule => rule.name === name);
-        if (index !== -1) {
-            this._rules.splice(index, 1);
-        }
-        return this;
-    }
 }
 
-class ObjectSchema<T extends Record<any, any>> extends Schema<T, any> {
+class ObjectSchema<T extends Record<any, Schema<T>>> extends Schema<T> {
     _schema: T;
 
     constructor(schema: T) {
-        super();
+        super({
+            type: "object",
+            check: (v: unknown) => typeof v === "object"
+        });
         this._schema = schema;
     }
 
     _parse(args: ParseInput): ParseReturn {
+        const _value = args.input;
         const result: ParseReturn = {
-            input: args,
-            valid: true
+            input: _value,
+            valid: true,
+            issues: []
         }
 
-        return result
-    };
-}
+        for (const key in this._schema) {
+            const _schema = this._schema[key]
+            const _result = _schema._parse({ input: _value ? _value[key] : _value })
 
-class StringSchema extends Schema<string, any> {
-    _parse(args: ParseInput): ParseReturn {
-        const result: ParseReturn = {
-            input: args,
-            valid: true
-        };
-
-        for (const rule of this._rules) {
-            if (!rule.cb(args.input)) {
-                this._addIssue({
-                    message: rule.message,
-                    name: rule.name,
-                    found: args.input,
-                })
+            if (!_result.valid) {
+                for (const issue of _result.issues) {
+                    this._addIssue(issue)
+                }
             }
         }
 
         result.issues = this._issues
-        if (result.issues) {
+        if (result.issues.length) {
             result.valid = false
         }
-
-        return result;
+        return result
     };
 }
 
+class StringSchema extends Schema<string> {
+    constructor() {
+        super({
+            type: "string",
+            check: (v: unknown) => typeof v === "string"
+        });
+    }
+
+    min(min: number) {
+        return this._addRule({
+            name: "min",
+            cb: (v: unknown) => typeof v === "string" && v.length >= min,
+            message: `Must have a minimum length of ${min}`
+        })
+    }
+}
+
+class ValidationError {
+    _result: ParseReturn
+    constructor(result: ParseReturn) {
+        this._result = result
+    }
+}
+
 export const v = {
-    object: (schema: Record<any, any>) => new ObjectSchema(schema),
-    string: () => new StringSchema()
+    object: (schema: Record<any, Schema<any>>) => new ObjectSchema(schema),
+    string: () => new StringSchema(),
+    ValidationError,
 }
