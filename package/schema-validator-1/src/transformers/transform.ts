c02c139bbbdcf5e088f66126f0eff085fe890cf6
polaski0 <meynard.trinidad44@gmail.com>
2024-10-28 23:15:34 +0800

    Removed old schema-validator

--- package/schema-validator-1/src/transformers/transform.ts
+++
@@ -1,8 +0,0 @@
-/**
-  * Used for mutating the current data.
-  */
-export function transform<TInput, TOutput = TInput>(cb: (input: TInput) => TOutput) {
-  return (input: TInput) => {
-    return cb(input)
-  }
-}
--- package/schema-validator-1/src/transformers/trim.ts
+++
@@ -1,7 +0,0 @@
-import { transform } from "./transform"
-
-export function trim<TInput extends string>() {
-  return transform((input: TInput) => {
-    return input.trim()
-  })
-}
